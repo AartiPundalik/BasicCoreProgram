2753c345eae22fa45cd7c7a84d4e2093ff5aad22
AartiPundalik <aartipundalik68@gmail.com>
2023-02-06 23:29:04 +0530

    [Aart] Add : UC3-PowerOTwo

---
+++ BasicCoreProgram/PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class PowerOfTwo
+    {
+        public void Power()
+        {
+            Console.WriteLine("Enter the power");
+            int powerNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < powerNum; i++)
+            {
+                int table = (int)Math.Pow(2, i);
+                Console.WriteLine(table);
+            }
+        }
+    }
+}
+    
+
